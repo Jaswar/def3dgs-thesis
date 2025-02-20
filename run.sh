61253cbc0c3bd0c949bdc9af469c2208c1305f5c
Jan Warchocki <janwarchocki@gmail.com>
2025-02-20 15:28:46 +0100

    Fix scaling

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 # python train.py -s ek_eg_sub/P18_06 -m output/ek_eg_3_ex_sub --eval
 # python train.py -s ek_eg_sub/P32_01 -m output/ek_eg_4_ex_sub --eval
 
-python train.py -s def3dgs_input/ek_eg_1_ex -m output/ek_eg_1_ex --eval
-python train.py -s def3dgs_input/ek_eg_2_ex -m output/ek_eg_2_ex --eval
-python train.py -s def3dgs_input/ek_eg_4_ex -m output/ek_eg_4_ex --eval
+python train.py -s def3dgs_input/ek_eg_1_ex -m output/ek_eg_1_ex_colfullinj --eval
+python train.py -s def3dgs_input/ek_eg_2_ex -m output/ek_eg_2_ex_colfullinj --eval
+python train.py -s def3dgs_input/ek_eg_4_ex -m output/ek_eg_4_ex_colfullinj --eval
